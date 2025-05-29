3d66da1ae2f5b6b78f9bc74c8368cac29795ff9e
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-05-29 22:45:34 +0300

    added allure attach

--- conftest.py
+++ conftest.py
@@ -1,11 +1,14 @@
 import os
 
+import allure
+import allure_commons
 import pytest
+
 from appium.options.android import UiAutomator2Options
 from appium.options.ios import XCUITestOptions
 from appium import webdriver
 
-from selene import browser
+from selene import browser, support
 
 import config
 
@@ -55,18 +58,52 @@ def mobile(request):
             }
         }
         )
+    with allure.step('init app session'):
+        browser.config.driver = webdriver.Remote(
+            config.driver_remote_url,
+            options=options)
 
-    browser.config.driver = webdriver.Remote(
-        config.driver_remote_url,
-        options=options)
-
-    # browser.config.driver_remote_url = config.driver_remote_url
-    # browser.config.driver_options = options
     browser.config.timeout = float(os.getenv('timeout', '10.0'))
+
+    browser.config._wait_decorator = support._logging.wait_with(
+        context=allure_commons._allure.StepContext
+    )
 
     yield
 
-    browser.quit()
+    allure.attach(
+        browser.driver.get_screenshot_as_png(),
+        name='screenshot',
+        attachment_type=allure.attachment_type.PNG,
+    )
+
+    allure.attach(
+        browser.driver.page_source,
+        name='screen xml dump',
+        attachment_type=allure.attachment_type.XML,
+    )
+
+    session_id = browser.driver.session_id
+
+    with allure.step('tear down app session'):
+        browser.quit()
+
+    import requests
+    bstack_session = requests.get(
+        f'https://api.browserstack.com/app-automate/sessions/{session_id}.json',
+        auth=(config.user_name, config.access_key),
+    ).json()
+    video_url = bstack_session['automation_session']['video_url']
+
+    allure.attach(
+        '<html><body>'
+        '<video width="100%" height="100%" controls autoplay>'
+        f'<source src="{video_url}" type="video/mp4">'
+        '</video>'
+        '</body></html>',
+        name='video_recording',
+        attachment_type=allure.attachment_type.HTML,
+    )
 
 
 ios = pytest.mark.parametrize('mobile', ['ios'], indirect=True)
