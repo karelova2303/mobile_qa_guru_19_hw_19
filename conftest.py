3063d5bac7baa54c9e9f4e6e7b41aa7982829ef4
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-05-29 23:29:39 +0300

    update conftest.py

--- conftest.py
+++ conftest.py
@@ -11,6 +11,7 @@ from appium import webdriver
 from selene import browser, support
 
 import config
+from mobile_hw_19 import utils
 
 
 @pytest.fixture(scope='function', params=['android', 'ios'])
@@ -71,39 +72,15 @@ def mobile(request):
 
     yield
 
-    allure.attach(
-        browser.driver.get_screenshot_as_png(),
-        name='screenshot',
-        attachment_type=allure.attachment_type.PNG,
-    )
-
-    allure.attach(
-        browser.driver.page_source,
-        name='screen xml dump',
-        attachment_type=allure.attachment_type.XML,
-    )
+    utils.allure.attach_bstack_screenshot()
+    utils.allure.attach_bstack_page_source()
 
     session_id = browser.driver.session_id
 
     with allure.step('tear down app session'):
         browser.quit()
 
-    import requests
-    bstack_session = requests.get(
-        f'https://api.browserstack.com/app-automate/sessions/{session_id}.json',
-        auth=(config.user_name, config.access_key),
-    ).json()
-    video_url = bstack_session['automation_session']['video_url']
-
-    allure.attach(
-        '<html><body>'
-        '<video width="100%" height="100%" controls autoplay>'
-        f'<source src="{video_url}" type="video/mp4">'
-        '</video>'
-        '</body></html>',
-        name='video_recording',
-        attachment_type=allure.attachment_type.HTML,
-    )
+    utils.allure.attach_bstack_video(session_id)
 
 
 ios = pytest.mark.parametrize('mobile', ['ios'], indirect=True)
