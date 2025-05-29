2feaaa18e204b0cdaf68216e0fdf7abda504cc88
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-05-29 23:32:21 +0300

    update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-user_name = os.getenv('user_name', 'USER_NAME')
-access_key = os.getenv('access_key', 'ACCESS_KEY')
-driver_remote_url = os.getenv('driver_remote_ur', 'DRIVER_REMOTE_URL')
+user_name = os.getenv('USER_NAME')
+access_key = os.getenv('ACCESS_KEY')
+driver_remote_url = os.getenv('DRIVER_REMOTE_URL')
